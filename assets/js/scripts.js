51c0892b255677d6f65f9479b51cbcba587b8772
Mauro Rocha <mauror@mauror.com.br>
2021-12-05 20:48:53 -0300

    Aula 08

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,7 @@
 /* Open/Close Cart Sidebar */
 var cartSidebar = document.querySelector('.cart-sidebar')
-function openSidebar() {
+function openSidebar(event) {
+    event.stopPropagation()
     cartSidebar.classList.add('cart-sidebar-open')
 }
 
@@ -14,10 +15,15 @@ btnCloseCart.addEventListener('click', closeSidebar)
 const btnAddMore = document.getElementById('btn-add-more')
 btnAddMore.addEventListener('click', closeSidebar)
 
+document.addEventListener('click', closeSidebar)
+cartSidebar.addEventListener('click', event => event.stopPropagation())
+
+const keyCart = 'productsCart'
+
 /* Fetch Products */
 const fetchProducts = () => {
     const groupRootEl = document.querySelector('#groups-root')      
-    fetch('http://127.0.0.1:5500/products.json')
+    fetch('/products.json')
     .then(response => response.json())
     .then(body => {   
         groupRootEl.innerHTML = ''     
@@ -75,12 +81,36 @@ const setupAddToCart = () => {
     })
 }
 
-const setupRemoveOfCart = () => {
+const handleKeydown = event => {
+    if (event.key == '-' || event.key == '.')
+        event.preventDefault()    
+}
+
+const handleUpdateQty = event => {
+    const { id } = event.target.dataset
+    const qty = parseInt(event.target.value)
+    if(qty > 0) {
+        const index = productsCart.findIndex(item => item.id == id)
+        productsCart[index].qty = qty
+        handleCartUpdate(false)
+    } else {
+        productsCart = productsCart.filter(product => product.id != id)
+        handleCartUpdate()
+    }    
+}
+
+const setupCartEvents = () => {
     const btnRemoveCartEls = document.querySelectorAll('.btn-remove-cart')    
     btnRemoveCartEls.forEach((btn) => {
         btn.addEventListener('click', removeOfCart)
     })    
-    console.log(btnRemoveCartEls)
+    //console.log(btnRemoveCartEls)
+    const inputQtyEls = document.querySelectorAll('.input-qty-cart')    
+    inputQtyEls.forEach(input => {
+        input.addEventListener('keydown', handleKeydown)
+        input.addEventListener('keyup', handleUpdateQty)
+        input.addEventListener('change', handleUpdateQty)
+    })
 }
 
 const removeOfCart = () => {        
@@ -92,7 +122,8 @@ const removeOfCart = () => {
     handleCartUpdate()
 }
 
-const handleCartUpdate = () => {
+const handleCartUpdate = (renderItems = true) => {    
+    localStorage.setItem(keyCart, JSON.stringify(productsCart))
     const badgeEl = document.querySelector('#btn-cart .badge')
     const emptyCartEl = document.querySelector('#empty-cart')
     const cartWithProducts = document.querySelector('#cart-with-products')
@@ -106,8 +137,9 @@ const handleCartUpdate = () => {
         badgeEl.innerHTML = totalCart
         cartWithProducts.classList.add('cart-with-products-show')
         emptyCartEl.classList.remove('empty-cart-show')
-        cartItensParent.innerHTML = ''        
-        productsCart.forEach(product => {
+        if (renderItems) {
+            cartItensParent.innerHTML = ''        
+            productsCart.forEach(product => {
             cartItensParent.innerHTML += `
                 <li class="cart-item">
                     <img src="${product.image}" alt="${product.name}" width="70" height="70">
@@ -115,13 +147,14 @@ const handleCartUpdate = () => {
                         <p class="h3">${product.name}</p>
                         <p class="price">R$ ${product.price.toLocaleString('pt-br', { minimumFractionDigits: 2})}</p>
                     </div>
-                    <input class="form-input" type="number" min="0" value="${product.qty}"/>
+                    <input class="form-input input-qty-cart" type="number" min="0" max="5" value="${product.qty}" data-id="${product.id}"/>
                     <button class="btn-remove-cart" data-id="${product.id}" >
                         <i class="fas fa-trash-alt"></i>
                     </button>
                 </li>`
-        }) 
-        setupRemoveOfCart()       
+            }) 
+            setupCartEvents()  
+        }               
         const totalPrice = productsCart.reduce((total, item) => total + item.qty * item.price, 0)         
         cartTotalValueEl.innerHTML = `R$ ${totalPrice.toLocaleString('pt-br', { minimumFractionDigits: 2})}`       
     } else {
@@ -132,6 +165,11 @@ const handleCartUpdate = () => {
 }
 
 //Chamar no inicio
-handleCartUpdate()
-
+const iniCart = () => {
+    const savedProducts = localStorage.getItem(keyCart)
+    if (savedProducts)
+        productsCart = JSON.parse(savedProducts)   
+    handleCartUpdate()  
+}
+iniCart()
 
