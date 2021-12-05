9b2d80f018ba0b6c615d6b63fd47eefca7b2e4c5
Mauro Rocha <mauror@mauror.com.br>
2021-12-05 18:52:21 -0300

    Aula 07

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -31,7 +31,13 @@ const fetchProducts = () => {
                                     <h3>${product.name}</h3>
                                     <p class="price">R$ ${product.price.toLocaleString('pt-br', { minimumFractionDigits: 2})}</p>
                                     ${description}
-                                    <button data-id="${product.id}" class="btn btn-main btn-block btn-add-cart">Adicionar</button>
+                                    <button 
+                                    data-id="${product.id}" 
+                                    data-name="${product.name}" 
+                                    data-image="${product.image}" 
+                                    data-price="${product.price}"                                     
+                                    class="btn btn-main btn-block btn-add-cart"
+                                    >Adicionar</button>
                                 </div>
                             </article>`
                 })
@@ -47,9 +53,19 @@ const fetchProducts = () => {
 fetchProducts()
 
 /* Add Products in Cart */
-const productsCart = []
+let productsCart = []
 const addToCart = (event) => {
-    console.log(event.target.dataset)
+    const product = event.target.dataset
+    const index = productsCart.findIndex(item => item.id == product.id)
+    if (index == -1) {
+        productsCart.push({
+            ...product, 
+            price: Number(product.price),
+            qty: 1 })    
+    } else {
+        productsCart[index].qty++
+    }    
+    handleCartUpdate()        
 }
 
 const setupAddToCart = () => {
@@ -57,7 +73,65 @@ const setupAddToCart = () => {
     btnAddCartEls.forEach(btn => {
         btn.addEventListener('click', addToCart)
     })
-    console.log(btnAddCartEls)
 }
+
+const setupRemoveOfCart = () => {
+    const btnRemoveCartEls = document.querySelectorAll('.btn-remove-cart')    
+    btnRemoveCartEls.forEach((btn) => {
+        btn.addEventListener('click', removeOfCart)
+    })    
+    console.log(btnRemoveCartEls)
+}
+
+const removeOfCart = () => {        
+    console.log(this)
+    /*
+    const { id } = this.dataset   
+    productsCart = productsCart.filter(product => product.id != id)
+    */
+    handleCartUpdate()
+}
+
+const handleCartUpdate = () => {
+    const badgeEl = document.querySelector('#btn-cart .badge')
+    const emptyCartEl = document.querySelector('#empty-cart')
+    const cartWithProducts = document.querySelector('#cart-with-products')
+    const cartItensParent = cartWithProducts.querySelector('ul')
+    const cartTotalValueEl = document.querySelector('#cart-total-value')
+    const totalCart = productsCart.reduce((total, item) => {
+        return total + item.qty
+    }, 0)    
+    if (totalCart > 0) {
+        badgeEl.classList.add('badge-show')
+        badgeEl.innerHTML = totalCart
+        cartWithProducts.classList.add('cart-with-products-show')
+        emptyCartEl.classList.remove('empty-cart-show')
+        cartItensParent.innerHTML = ''        
+        productsCart.forEach(product => {
+            cartItensParent.innerHTML += `
+                <li class="cart-item">
+                    <img src="${product.image}" alt="${product.name}" width="70" height="70">
+                    <div>
+                        <p class="h3">${product.name}</p>
+                        <p class="price">R$ ${product.price.toLocaleString('pt-br', { minimumFractionDigits: 2})}</p>
+                    </div>
+                    <input class="form-input" type="number" min="0" value="${product.qty}"/>
+                    <button class="btn-remove-cart" data-id="${product.id}" >
+                        <i class="fas fa-trash-alt"></i>
+                    </button>
+                </li>`
+        }) 
+        setupRemoveOfCart()       
+        const totalPrice = productsCart.reduce((total, item) => total + item.qty * item.price, 0)         
+        cartTotalValueEl.innerHTML = `R$ ${totalPrice.toLocaleString('pt-br', { minimumFractionDigits: 2})}`       
+    } else {
+        badgeEl.classList.remove('badge-show')
+        emptyCartEl.classList.add('empty-cart-show')
+        cartWithProducts.classList.remove('cart-with-products-show')
+    }
+}
+
+//Chamar no inicio
+handleCartUpdate()
 
 
